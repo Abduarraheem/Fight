c7e56107e3ea950d2bb51231053cfc11d05c4b61
cilantrose <aliu5@uoregon.edu>
2022-05-08 16:47:27 -0700

    moving camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+
+    public Transform cam;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,21 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float minX = 0.0f;
+        float minZ = 0.0f;
+        float maxX = 0.0f;
+        float maxZ = 0.0f;
+        GameObject[] foundObjects = GameObject.FindGameObjectsWithTag("Player");
+        for (int i = 0; i < foundObjects.Length; i ++)
+        {
+            Vector3 t = foundObjects[i].transform.position;
+            if (t.x < minX) minX = t.x;
+            if (t.z < minZ) minZ = t.z;
+            if (t.x > maxX) maxX = t.x;
+            if (t.z > maxZ) maxZ = t.z;
+        }
+        float centerX = (minX + maxX) / 2;
+        float centerZ = (minZ + maxZ) / 2;
+        cam.position = new Vector3(centerX, 6.0f + centerZ, -7.0f);
     }
 }
--- Assets/Scripts/OnStart.cs
+++ Assets/Scripts/OnStart.cs
@@ -6,12 +6,13 @@ using UnityEngine.InputSystem;
 public class OnStart : MonoBehaviour
 {
     
-    public GameObject playerPrefab;
+    public GameObject player1Prefab;
+    public GameObject player2Prefab;
     // Start is called before the first frame update
     void Start()
     {
-        PlayerInput.Instantiate(playerPrefab, controlScheme: "Keyboard", pairWithDevice: Keyboard.current);
-        PlayerInput.Instantiate(playerPrefab, controlScheme: "Player 2", pairWithDevice: Keyboard.current);
+        PlayerInput.Instantiate(player1Prefab, controlScheme: "Keyboard", pairWithDevice: Keyboard.current);
+        PlayerInput.Instantiate(player2Prefab, controlScheme: "Player 2", pairWithDevice: Keyboard.current);
     }
 
     // Update is called once per frame
