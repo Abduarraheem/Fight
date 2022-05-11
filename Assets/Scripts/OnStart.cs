f9f315870c6145b483c9f74f0b4cd6b85375f7ea
cilantrose <aliu5@uoregon.edu>
2022-05-10 17:31:32 -0700

    multiple spawnpoints

--- Assets/Scripts/OnStart.cs
+++ Assets/Scripts/OnStart.cs
@@ -8,11 +8,17 @@ public class OnStart : MonoBehaviour
     
     public GameObject player1Prefab;
     public GameObject player2Prefab;
+    public Vector3[] validSpawns;
     // Start is called before the first frame update
     void Start()
     {
         PlayerInput.Instantiate(player2Prefab, controlScheme: "Keyboard", pairWithDevice: Keyboard.current);
         PlayerInput.Instantiate(player2Prefab, controlScheme: "Player 2", pairWithDevice: Keyboard.current);
+        GameObject[] foundObjects = GameObject.FindGameObjectsWithTag("Player");
+        for (int i = 0; i < foundObjects.Length; i ++)
+        {
+            foundObjects[i].transform.position = validSpawns[i];
+        }
     }
 
     // Update is called once per frame
